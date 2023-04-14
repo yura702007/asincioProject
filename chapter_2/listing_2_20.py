70d59671644fa54085891c8f63aea1623b9fada5
yura702007 <82802568+yura702007@users.noreply.github.com>
2023-04-14 13:55:11 +0300

    use block API

---
+++ chapter_2/listing_2_20.py
@@ -0,0 +1,25 @@
+"""
+Неправильное использование блокирующего как API сопрограммы
+"""
+import asyncio
+import requests
+from util import async_timed
+
+
+@async_timed()
+async def get_example_status() -> int:
+    return requests.get('https://www.example.com').status_code
+
+
+@async_timed()
+async def main():
+    task_1 = asyncio.create_task(get_example_status())
+    task_2 = asyncio.create_task(get_example_status())
+    task_3 = asyncio.create_task(get_example_status())
+    await task_1
+    await task_2
+    await task_3
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
