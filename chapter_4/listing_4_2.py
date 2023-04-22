cde2af81c6b78f12ca6a4d746214e9cae86178ff
yura702007 <yura702007@gmail.com>
2023-04-22 18:03:13 +0300

    creating web-request with aiohttp

---
+++ chapter_4/listing_4_2.py
@@ -0,0 +1,24 @@
+"""
+Отправка веб-запроса с помощью aiohttp
+"""
+import asyncio
+from aiohttp import ClientSession
+from util import async_timed
+
+
+@async_timed()
+async def fetch_status(session: ClientSession, url: str) -> int:
+    async with session.get(url) as result:
+        return result.status
+
+
+@async_timed()
+async def main():
+    async with ClientSession() as session:
+        url = 'https://www.example.com'
+        status = await fetch_status(session, url)
+        print(f'Состояние для {url} было равно{status}')
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
