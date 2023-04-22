e12424338bf81c90febeda7d7bd5934addee58b9
yura702007 <yura702007@gmail.com>
2023-04-22 18:31:23 +0300

    added fetch_status() in __init__.py

---
+++ chapter_4/__init__.py
@@ -0,0 +1,8 @@
+from aiohttp import ClientSession
+from util import async_timed
+
+
+@async_timed()
+async def fetch_status(session: ClientSession, url: str) -> int:
+    async with session.get(url) as result:
+        return result.status
--- chapter_4/listing_4_2.py
+++ chapter_4/listing_4_2.py
@@ -4,12 +4,13 @@
 import asyncio
 from aiohttp import ClientSession
 from util import async_timed
+from chapter_4 import fetch_status
 
 
-@async_timed()
-async def fetch_status(session: ClientSession, url: str) -> int:
-    async with session.get(url) as result:
-        return result.status
+# @async_timed()
+# async def fetch_status(session: ClientSession, url: str) -> int:
+#     async with session.get(url) as result:
+#         return result.status
 
 
 @async_timed()
