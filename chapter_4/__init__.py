44838682bf0707c886d681b4e69f6ff454ac384f
yura702007 <yura702007@gmail.com>
2023-04-28 01:58:26 +0300

    adding delay argument in fetch_status function

--- chapter_4/__init__.py
+++ chapter_4/__init__.py
@@ -1,8 +1,10 @@
+import asyncio
 from aiohttp import ClientSession
 from util import async_timed
 
 
 @async_timed()
-async def fetch_status(session: ClientSession, url: str) -> int:
+async def fetch_status(session: ClientSession, url: str, delay: int) -> int:
+    await asyncio.sleep(delay)
     async with session.get(url) as result:
         return result.status
