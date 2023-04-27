4e7f4de3d67f3c3d2e5c62cf4ff9c563785eefa9
yura702007 <yura702007@gmail.com>
2023-04-28 00:09:51 +0300

    using asyncio.gather(*tasks, return_exceptions=False)

---
+++ chapter_4/listing_4_8.py
@@ -0,0 +1,20 @@
+"""
+Обработка исключений при использовании gather
+"""
+import asyncio
+from aiohttp import ClientSession
+from util import async_timed
+from chapter_4 import fetch_status
+
+
+@async_timed()
+async def main():
+    async with ClientSession() as session:
+        urls = ['https://example.com', 'python://example.com']
+        tasks = [fetch_status(session=session, url=url) for url in urls]
+        status_codes = await asyncio.gather(*tasks, return_exceptions=False)
+        print(status_codes)
+
+
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(main())
