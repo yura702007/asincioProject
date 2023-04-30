fce81c3f630160e52e6c9145b235b333742b7fdf
yura702007 <yura702007@gmail.com>
2023-04-30 16:36:50 +0300

    bag fix in file sql-query's

--- chapter_5/listing_5_2.py
+++ chapter_5/listing_5_2.py
@@ -37,9 +37,12 @@ CREATE_SKU_TABLE = """
         product_id INT NOT NULL,
         product_size_id INT NOT NULL,
         product_color_id INT NOT NULL,
-        FOREIGN KEY (product_id) REFERENCES (product_id),
-        FOREIGN KEY (product_size_id) REFERENCES (product_size_id),
-        FOREIGN KEY (product_color_id) REFERENCES (product_color_id)
+        FOREIGN KEY (product_id)
+        REFERENCES product(product_id),
+        FOREIGN KEY (product_size_id)
+        REFERENCES product_size(product_size_id),
+        FOREIGN KEY (product_color_id)
+        REFERENCES product_color(product_color_id)
     );
 """
 
