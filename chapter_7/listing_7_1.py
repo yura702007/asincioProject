b09a89e95c8025eed915df7aca81df28a6c6bf0e
yura702007 <yura702007@gmail.com>
2023-05-21 11:28:31 +0300

    using Thread() for creating many threads echo-server

---
+++ chapter_7/listing_7_1.py
@@ -0,0 +1,27 @@
+"""
+Многопоточный эхо-сервер
+"""
+from threading import Thread
+import socket
+
+
+def echo(client: socket):
+    while True:
+        data = client.recv(2048)
+        print(f'Получено: {data}, отправляю!')
+        client.sendall(data)
+
+
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server.bind(('127.0.0.1', 8080))
+        server.listen()
+        while True:
+            connection, _ = server.accept()
+            thread = Thread(target=echo, args=(connection,))
+            thread.start()
+
+
+if __name__ == '__main__':
+    main()
