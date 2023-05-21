cb0653dc5f05e0156d9ff0b34aa193cc9ed93c4a
yura702007 <yura702007@gmail.com>
2023-05-21 15:25:01 +0300

    using pool threads

---
+++ chapter_7/listing_7_4.py
@@ -0,0 +1,19 @@
+"""
+Выполнение запросов с помощью пула потоков
+"""
+import time
+from concurrent.futures.thread import ThreadPoolExecutor
+from chapter_7.listing_7_3 import get_status_code
+
+URL = 'https://www.example.com'
+
+if __name__ == '__main__':
+    start = time.time()
+
+    with ThreadPoolExecutor() as pool:
+        urls = [URL for _ in range(1000)]
+        results = pool.map(get_status_code, urls)
+        for result in results:
+            print(result)
+
+    print(f'Выполнение 1000 запросов завершено за {time.time() - start}с.')
