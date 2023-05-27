7b7e4cb5c3e55bcf2ed140409b167bc83a5e5723
yura702007 <yura702007@gmail.com>
2023-05-27 19:00:40 +0300

    using numpy for calculate mediums in big matrix

---
+++ chapter_7/listing_7_18.py
@@ -0,0 +1,16 @@
+"""
+Вычисление средних в большой матрице с помощью numpy
+"""
+import numpy as np
+import time
+
+
+data_points = 4 * 10 ** 8
+rows = 50
+columns = data_points // rows
+
+matrix = np.arange(data_points).reshape(rows, columns)
+
+start = time.time()
+result = np.mean(matrix, axis=1)
+print(time.time() - start)
