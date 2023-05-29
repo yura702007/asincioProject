9158475955f9f67fab2f1bf3a592248219ba096f
yura702007 <82802568+yura702007@users.noreply.github.com>
2023-05-29 12:05:36 +0300

    OSError

--- chapter_8/listing_8_5.py
+++ chapter_8/listing_8_5.py
@@ -3,6 +3,7 @@
 """
 import asyncio
 from asyncio import StreamReader
+from util import delay
 import sys
 
 
@@ -12,3 +13,18 @@ async def create_std_reader() -> StreamReader:
     loop = asyncio.get_running_loop()
     await loop.connect_read_pipe(lambda: protocol, sys.stdin)
     return stream_reader
+
+
+async def main():
+    stdin_reader = await create_std_reader()
+    while True:
+        delay_time: str = await stdin_reader.readline()
+        if delay_time:
+            await asyncio.create_task(delay(int(delay_time)))
+        else:
+            print('EXIT')
+            break
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
