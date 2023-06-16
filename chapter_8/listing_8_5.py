f4691f3d82884aace0ed5b7592e34808ef1e5772
yura702007 <82802568+yura702007@users.noreply.github.com>
2023-06-16 12:57:26 +0300

    OSError

--- chapter_8/listing_8_5.py
+++ chapter_8/listing_8_5.py
@@ -1,46 +1,18 @@
 """
 Асинхронный читатель стандартного ввода
 """
-import sys
 import asyncio
-from concurrent.futures import Future
-from asyncio import AbstractEventLoop
-from threading import Thread
-from util import delay, async_timed
+from asyncio import StreamReader
+import sys
 
 
-def reader_stdin() -> int:
-    return int(input('Введите количество секунд: '))
-
-
-# async def create_std_reader():
-#     stream_reader = asyncio.StreamReader()
-#     protocol = asyncio.StreamReaderProtocol(stream_reader=stream_reader)
-#     loop = asyncio.get_running_loop()
-#     await loop.connect_read_pipe(lambda: protocol, sys.stdin)
-#     return stream_reader
-
-
-class ThreadedEventLoop(Thread):
-    def __init__(self, loop: AbstractEventLoop):
-        super().__init__()
-        self._loop = loop
-        self.daemon = True
-
-    def run(self):
-        self._loop.run_forever()
-
-
-@async_timed()
-async def main():
-    tasks = []
-    while True:
-        try:
-            tasks.append(asyncio.create_task(delay(reader_stdin())))
-        except ValueError:
-            break
-    await asyncio.gather(*tasks)
+async def create_std_reader() -> StreamReader:
+    stream_reader = StreamReader()
+    protocol = asyncio.StreamReaderProtocol(stream_reader=stream_reader)
+    loop = asyncio.get_running_loop()
+    await loop.connect_read_pipe(lambda: protocol, sys.stdin)
+    return stream_reader
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    asyncio.run(create_std_reader())
